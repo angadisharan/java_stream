bb85f9507237d36c91b51a3d4b30c3e51af39737
Sharan <sharan@mathrusoft.com>
2026-04-19 17:37:18 +0530

    immutable

--- src/main/java/org/cleancoding/TestMe.java
+++ src/main/java/org/cleancoding/TestMe.java
@@ -1,21 +1,37 @@
 package org.cleancoding;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TestMe {
 
 
     public static void main(String args[]) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 5, 5, 6, 6 , 2 , 4);
-        int[] numbers = {10, 4, 22, 5, 90, 31};
+        List<String> list = Arrays.asList("apple", "banana", "apple", "orange", "banana", "apple");
 
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        LinkedHashMap map =
+                list.stream()
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                        .entrySet()
+                        .stream()
+                        .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                (a, b) -> a,
+                                LinkedHashMap::new
+                        ));
+
+        System.out.println(
+                map
+
+
+        );
+
 
     }
 
---
+++ src/main/java/org/cleancoding/immutable/Address.java
@@ -0,0 +1,19 @@
+package org.cleancoding.immutable;
+
+final class Address {
+    private final String city;
+    private final String street;
+
+    public Address(String city, String street) {
+        this.city = city;
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+}
